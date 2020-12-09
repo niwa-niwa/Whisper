0d5be118104f9075989c2f66e845e0196947fa7e
niwa-niwa <niwayama1101@gmail.com>
2020-12-09 23:08:49 +0900

    added an if sentence for a user that has not posts7

--- posts/views.py
+++ posts/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
+from django.contrib.auth.decorators import login_required
+
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
-
 class PostsListView(generic.ListView):
+    context_object_name = 'posts'
     model = Post
     template_name = 'posts_list.html'
     paginate_by = 20
 
     def get_queryset(self):
         posts = Post.objects.filter(user=self.request.user).order_by('-created_at')
-        print(str(posts))
         return posts
     
