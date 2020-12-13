348456717d88c97c63a8607c217805e0e43a387d
niwa-niwa <niwayama1101@gmail.com>
2020-12-14 00:23:31 +0900

    made one to many from users

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from accounts.models import CustomUser
 
 
 class Post(models.Model):
-    user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.PROTECT)
+    user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.PROTECT, related_name='post')
     content = models.TextField(verbose_name='本文', blank=False, null=False)
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
