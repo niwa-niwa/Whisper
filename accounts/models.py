2e2d099272bd6c9e84ff8863b5d1571d333ad6cf
niwa-niwa <niwayama1101@gmail.com>
2020-12-19 20:30:06 +0900

    made UsersRelation

--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
 
+    followings = models.ManyToManyField("CustomUser", verbose="フォロー中のユーザー", through="UsersRelation", related_name="+", through_fields=('following', 'follower'))
+
+    followers = models.ManyToManyField("CustomUser", verbose="フォローされているユーザー", through="UsersRelation", related_name="+", through_fields=('follower', 'following'))
+    
     class Meta:
         verbose_name_plural = 'CustomUser'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from accounts.models import CustomUser
 from django.db import models
 
 # Create your models here.
+class UsersRelation(models.Model):
+    follower = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='following')
+
+    following = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='follower')
+
+    class Meta:
+        unique_together = ('follower', 'following')
