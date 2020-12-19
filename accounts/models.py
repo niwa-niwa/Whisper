8a9ade4e3cf7a12ef3ed375a286a6a3edbeaa856
niwa-niwa <niwayama1101@gmail.com>
2020-12-19 21:53:32 +0900

    fixed syntax error for makemaigration

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from users.models import UsersRelation
 
 
 class CustomUser(AbstractUser):
-
-    followings = models.ManyToManyField("CustomUser", verbose="フォロー中のユーザー", through="UsersRelation", related_name="+", through_fields=('following', 'follower'))
-
-    followers = models.ManyToManyField("CustomUser", verbose="フォローされているユーザー", through="UsersRelation", related_name="+", through_fields=('follower', 'following'))
     
+    followings = models.ManyToManyField('CustomUser', verbose_name="フォロー中のユーザー", through=UsersRelation, related_name='+',  through_fields=('follower', 'following'))
+
+    followers = models.ManyToManyField('CustomUser', verbose_name="フォローされているユーザー", through=UsersRelation, related_name='+', through_fields=('following', 'follower'))
+
     class Meta:
         verbose_name_plural = 'CustomUser'
+
--- users/models.py
+++ users/models.py
@@ -1,11 +1,13 @@
-from accounts.models import CustomUser
 from django.db import models
+
 
 # Create your models here.
 class UsersRelation(models.Model):
-    follower = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='following')
+    
+    # import CustomUserで参照しようとするとエラーが出るため、第一引数はpathを使用すること
+    follower = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name='followers_relation')
 
-    following = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='follower')
+    following = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE, related_name='followings_relation')
 
     class Meta:
         unique_together = ('follower', 'following')
