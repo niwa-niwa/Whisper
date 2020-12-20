b4684fb5959a2620d0fcf8f83e6f25445cf07f77
niwa-niwa <niwayama1101@gmail.com>
2020-12-20 20:56:55 +0900

    memo

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path('users/', views.UsersIndex.as_view(), name="users_index"),
+    # path ('/', views.UserTimeLine.as_views(), name='user_timeline') #フォロしてるユーザーを含めたタイムライン
     # path('home/', views.UserHome.as_views(), name='user_home')
     # path('settings/', views.UserSettings.as_views(), name='user_settings')
+    path('users/', views.UsersIndex.as_view(), name="users_index"),
     path('<slug:username>/', views.UserPostsListAndCreate.as_view(), name='user_page'),
     path('<slug:username>/followings/', views.UserFollowings.as_view(), name='user_followings'),
     path('<slug:username>/followers/', views.UserFollowers.as_view(), name='user_followers'),
