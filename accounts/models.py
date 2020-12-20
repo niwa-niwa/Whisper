f808ad6e2148dd3c3d2755c295fc8673f135c635
niwa-niwa <niwayama1101@gmail.com>
2020-12-20 12:31:38 +0900

    made methods in user-model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from users.models import UsersRelation
+from django.apps import apps
 
 
 class CustomUser(AbstractUser):
@@ -11,3 +12,24 @@ class CustomUser(AbstractUser):
 
     class Meta:
         verbose_name_plural = 'CustomUser'
+
+    
+    # ユーザーの投稿をすべて取得
+    def getPosts(self):
+        post_model = apps.get_model('posts', 'Post')
+        posts = post_model.objects.filter(user=self).order_by('-created_at')
+        return posts
+
+
+    # ユーザーがフォローしているユーザー取得
+    def getFollowings(self):
+        users_relation = apps.get_model('users', 'UsersRelation')
+        followings = users_relation.objects.filter(follower=self)
+        return followings
+
+    # ユーザーをフォローしているユーザー取得
+    def getFollowers(self):
+        users_relation = apps.get_model('users', 'UsersRelation')
+        followers = users_relation.objects.filter(following=self)
+        return followers
+
