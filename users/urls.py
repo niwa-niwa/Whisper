adbe74f14980fc4788d03f1e4e12e297a3692b98
niwa-niwa <niwayama1101@gmail.com>
2020-12-20 20:32:41 +0900

    refactoried url followings & followers

--- users/urls.py
+++ users/urls.py
@@ -5,9 +5,11 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('', views.UsersIndex.as_view(), name="users_index"),
+    # path('home/', views.UserHome.as_views(), name='user_home')
+    # path('settings/', views.UserSettings.as_views(), name='user_settings')
     path('<slug:username>/', views.UserPostsListAndCreate.as_view(), name='user_page'),
-    path('<int:pk>/followings/', views.UserFollowings.as_view(), name='user_followings'),
-    path('<int:pk>/followers/', views.UserFollowers.as_view(), name='user_followers'),
+    path('<slug:username>/followings/', views.UserFollowings.as_view(), name='user_followings'),
+    path('<slug:username>/followers/', views.UserFollowers.as_view(), name='user_followers'),
     path('follow/<int:pk>/', views.followings, name="user_following"),
     path('unfollow/<int:pk>/', views.unfollowings, name="user_unfollowing"),
 ]
